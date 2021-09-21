87412f3a5761ce8d9df06a9a61812b8d542a93a5
Fredrik Christenson <fredrik.christenson@volvocars.com>
2021-09-21 10:21:54 +0200

    Added React.StrictMode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { HelloWorld } from "../src/components/HelloWorld";
 import "../public/css/styles.css";
+import React from "react";
 
 function HomePage() {
-  return <HelloWorld />;
+  return (
+    <React.StrictMode>
+      <HelloWorld />
+    </React.StrictMode>
+  );
 }
 
 export default HomePage;

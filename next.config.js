65f0aaa326ff2f9846f354809e2a49e7eaef7d25
Gabriel Lundgren <gabriel.lundgren@outsystems.com>
2022-10-03 15:19:54 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const path = require("path");
 
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/Index",
+        permanent: true,
+      },
+    ];
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",

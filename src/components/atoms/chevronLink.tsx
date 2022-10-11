af19fc252d6e3dd57e1b64ec1166e5b65b82b476
Manjodh Singh <manjodheriksingh@gmail.com>
2022-10-11 11:49:26 +0200

    create chevron link

---
+++ src/components/atoms/chevronLink.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { ReactNode } from 'react'
+import { Link } from 'vcc-ui'
+
+type Props = {
+  children: ReactNode
+  href: string
+  external?: boolean
+}
+
+export const ChevronLink = ({ children, href, external = false }: Props) => {
+  const { push } = useRouter()
+
+  const navigationProp = external ? { href } : { onClick: () => push(href) }
+  return (
+    <Link style={{ display: 'flex', alignItems: 'center' }} {...navigationProp}>
+      {children}
+      <Image
+        src='/chevron-small.svg'
+        width={15}
+        height={15}
+        style={{ marginLeft: 6 }}
+        alt='arrow'
+      />
+    </Link>
+  )
+}

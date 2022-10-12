7293102a43d2484e890c68fc9a609d996410c0c5
Manjodh Singh <manjodheriksingh@gmail.com>
2022-10-12 05:13:34 +0200

    ssr car scrollers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,53 @@
 import type { NextPage } from 'next'
-import { useEffect, useMemo, useState } from 'react'
+import dynamic from 'next/dynamic'
+import { useMemo, useState } from 'react'
 import { Block, SelectInput, Spinner } from 'vcc-ui'
 
 import { fetchCars } from '../api/cars'
-import { CarScrollerDesktop } from '../components/screens/carScroller/carScrollerDesktop'
-import { CarScrollerMobile } from '../components/screens/carScroller/carScrollerMobile'
 import { useIsMobile } from '../hooks/useIsMobile'
 import { CarType } from '../types/cars'
 
-const Home: NextPage = () => {
-  const [cars, setCars] = useState<CarType[]>([])
-  const [availableBodyTypes, setAvailableBodyTypes] = useState<string[]>([])
-  const [filter, setFilter] = useState('all')
+const CarScrollerDesktop = dynamic(
+  () =>
+    import('../components/screens/carScroller/carScrollerDesktop').then(
+      (mod) => mod.CarScrollerDesktop
+    ),
+  {
+    ssr: false,
+  }
+)
+const CarScrollerMobile = dynamic(
+  () =>
+    import('../components/screens/carScroller/carScrollerMobile').then(
+      (mod) => mod.CarScrollerMobile
+    ),
+  {
+    ssr: false,
+  }
+)
+const DEFAULT_FILTER = 'all'
+
+type Props = {
+  cars: CarType[]
+  availableFilters: string[]
+}
+const Home: NextPage<Props> = ({ cars, availableFilters }) => {
+  const [availableBodyTypes] = useState<string[]>([
+    DEFAULT_FILTER,
+    ...availableFilters.filter(
+      (bodyType, index) => availableFilters.indexOf(bodyType) === index
+    ),
+  ])
+  const [activeFilter, setActiveFilter] = useState<string>(DEFAULT_FILTER)
 
   const { isMobile } = useIsMobile()
 
-  useEffect(() => {
-    const handleFetch = async () => {
-      const cars = await fetchCars()
-      if (cars.length !== 0) {
-        setCars(cars)
-        const carBodyTypes = cars.map((car) => car.bodyType)
-        setAvailableBodyTypes([
-          'all',
-          ...carBodyTypes.filter(
-            (bodyType, index) => carBodyTypes.indexOf(bodyType) === index
-          ),
-        ])
-      }
-    }
-    handleFetch()
-  }, [])
-
   const filteredCars = useMemo(() => {
-    if (filter === 'all') {
+    if (activeFilter === DEFAULT_FILTER) {
       return cars
     }
-    return cars.filter((car) => car.bodyType === filter)
-  }, [cars, filter])
+    return cars.filter((car) => car.bodyType === activeFilter)
+  }, [cars, activeFilter])
 
   const CarScroller = useMemo(
     () =>
@@ -62,8 +72,8 @@ const Home: NextPage = () => {
       {availableBodyTypes.length !== 0 ? (
         <SelectInput
           label={'Body Types'}
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
+          value={activeFilter}
+          onChange={(e) => setActiveFilter(e.target.value)}
         >
           {availableBodyTypes.map((bodyType) => (
             <option key={bodyType} value={bodyType}>
@@ -75,6 +85,16 @@ const Home: NextPage = () => {
       {cars.length !== 0 ? CarScroller : <Spinner />}
     </Block>
   )
+}
+
+export async function getServerSideProps() {
+  const cars = await fetchCars()
+  return {
+    props: {
+      cars,
+      availableFilters: cars.map((car) => car.bodyType),
+    },
+  }
 }
 
 export default Home

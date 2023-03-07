af741ca586107430473bc82714f852e52950277f
Måns Dahlström <mans@mansdahlstrom.se>
2023-03-07 14:29:03 +0100

    Setup vcc-ui Config provider for Next Links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { StyleProvider, ThemePicker } from 'vcc-ui';
+import NextLink from 'next/link';
+import { ConfigProvider, Link, StyleProvider, ThemePicker } from 'vcc-ui';
 
 import '../styles/global.scss';
 
+type LinkProps = React.ComponentProps<typeof Link>;
+const config = {
+  linkComponent: ({ children, href, ...linkProps }: LinkProps) => {
+    // we only want to use next/link for internal links
+    // external links are better served with an basic a tag
+    if (typeof href === 'object' || href?.indexOf('/') === 0) {
+      return (
+        <NextLink href={href}>
+          <a {...linkProps}>{children}</a>
+        </NextLink>
+      );
+    }
+
+    return (
+      <a href={href} {...linkProps}>
+        {children}
+      </a>
+    );
+  },
+};
+
 function App({ Component, pageProps }: AppProps) {
   return (
-    <StyleProvider>
-      <ThemePicker variant="light">
-        <Component {...pageProps} />
-      </ThemePicker>
-    </StyleProvider>
+    <ConfigProvider config={config}>
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <Component {...pageProps} />
+        </ThemePicker>
+      </StyleProvider>
+    </ConfigProvider>
   );
 }
 

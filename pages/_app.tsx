066435b17eb0718f5dd09efb483bdd4ddf9c3df4
Daniel Kiefer <danielfmpinho@gmail.com>
2023-07-04 17:52:09 -0300

    app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import { HelloWorld } from "../src/components/HelloWorld";
-import "../public/css/styles.css";
 import React from "react";
+import type { AppProps } from "next/app";
+import { StyleProvider, ThemePicker } from "vcc-ui";
+import "../public/css/styles.css";
 
-function HomePage() {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <React.StrictMode>
-      <HelloWorld />
-    </React.StrictMode>
+    <StyleProvider>
+      <ThemePicker variant="light">
+        <Component {...pageProps} />
+      </ThemePicker>
+    </StyleProvider>
   );
-}
+};
 
-export default HomePage;
+export default MyApp;
